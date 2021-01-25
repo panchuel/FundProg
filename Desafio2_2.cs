bc6fab3bd6f0e4ec7e2d3988fbf1ba1f1ad26340
panchuel <66889936+panchuel@users.noreply.github.com>
2021-01-25 12:09:22 -0500

    Update Desafio2_2.cs

--- Desafio2_2.cs
+++ Desafio2_2.cs
@@ -6,22 +6,27 @@
 	public static void Main()
 	{
 		//Entradas
-		Console.WriteLine("Agregue el valor de t");
-		double t = double.Parse(Console.ReadLine());
+			//Para t
+			Console.WriteLine("Agregue el valor de t");
+			double t = double.Parse(Console.ReadLine());
+			//Para a
+			Console.WriteLine("Agregue el valor de a");
+			double a = double.Parse(Console.ReadLine());
 
-		Console.WriteLine("Agregue el valor de a");
-		double a = double.Parse(Console.ReadLine());
-
-		//Angulo c
-		double c = 180 - 90 - a;
-		Console.WriteLine("El angulo a es " + c);
-
-		//Para el lado z (aplicando ley de seno)
-		double z = t * Math.Sin(a);
-		Console.WriteLine("El lado z es " + z);
-
-		//Para el lado  y 
-		double y = Math.Sqrt((t * t) - (z * z));
-		Console.WriteLine("EL lado y es " + y);
+		//Procedimiento
+			//Angulo c
+			double c = 180 - 90 - a;			
+			//Para el lado z (aplicando ley de seno)
+			double z = t * Math.Sin(a);			
+			//Para el lado  y 
+			double y = Math.Sqrt((t * t) - (z * z));			
+		
+		//Salida
+			//Para el angulo c
+			Console.WriteLine("El angulo a es " + c);
+			//Para z
+			Console.WriteLine("El lado z es " + z);
+			//Para el lado y
+			Console.WriteLine("EL lado y es " + y);
 	}
 }
