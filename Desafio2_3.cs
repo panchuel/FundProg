378cecb6f2514f852cf0ddd3da9ada475e6cbc0f
panchuel <66889936+panchuel@users.noreply.github.com>
2021-01-25 12:19:07 -0500

    Update Desafio2_3.cs

--- Desafio2_3.cs
+++ Desafio2_3.cs
@@ -6,27 +6,31 @@ class Program
 	static void Main()
     {
 		//Entradas
-		Console.WriteLine("Agregue el valor de z");
-		double z = double.Parse(Console.ReadLine());
+			//Para z
+			Console.WriteLine("Agregue el valor de z");
+			double z = double.Parse(Console.ReadLine());
+			//Para c
+			Console.WriteLine("Agregue el valor de c");
+			double c = double.Parse(Console.ReadLine());
 
-		Console.WriteLine("Agregue el valor de c");
-		double c = double.Parse(Console.ReadLine());
-
-		//Angulo a
-		double a = 180 - 90 - c;
-		Console.WriteLine("El angulo a es " + a);
-
-		//Cambio a radianes
-		c = c * Math.PI / 180;
-		a = a * Math.PI / 180;
-
-		//Para el lado y (aplicando ley de seno)
-		double y = z * Math.Sin(a) / Math.Sin(c);
-		Console.WriteLine("El lado y es " + y);
-
-		//Para la hipotenusa (usando pitagoras)
-		double t = Math.Sqrt((z * z) + (y * y));
-		Console.WriteLine("La hipotenusa es " + t);	
+		//Procedimiento
+			//Angulo a
+			double a = 180 - 90 - c;
+			//Cambio a radianes
+			cR = c * Math.PI / 180;
+			aR = a * Math.PI / 180;
+			//Para el lado y (aplicando ley de seno)
+			double y = z * Math.Sin(aR) / Math.Sin(cR);
+			//Para la hipotenusa (usando pitagoras)
+			double t = Math.Sqrt((z * z) + (y * y));			
+		
+		//Salida
+			//Para a
+			Console.WriteLine("El angulo a es " + a);
+			//Para y
+			Console.WriteLine("El lado y es " + y);
+			//Para t
+			Console.WriteLine("La hipotenusa es " + t);
 	}
 }
 
