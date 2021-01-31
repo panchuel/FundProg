d712e95864ac5a889926a548aedd1c45965f6494
panchuel <66889936+panchuel@users.noreply.github.com>
2021-01-31 02:58:16 -0500

    Add files via upload

---
+++ Calculo_cuota_moderadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculo_cuota_moderadora
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Entradas
+            int smmv = 908526;
+            Console.WriteLine("Ingrese su salario");
+            int salario = int.Parse(Console.ReadLine());
+
+            //Calculo de tarifas
+            if (salario <= smmv * 2)
+            {
+                Console.WriteLine("A usted le corresponde la tarifa A");
+            }
+            else if (salario > smmv * 2 && salario < smmv * 5)
+            {
+                Console.WriteLine("A usted le corresponde la tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("A usted le corresponde la tarifa C");
+            }
+        }
+    }
+}
---
+++ Calculo_de_deducciones_salariales.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Calculo_de_deducciones_salariales
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Entradas
+            Console.WriteLine("Ingrese su salario");
+            int salario_mensual = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su tipo de contrato: (1) para dependiente, (2) para independiente");
+            int tipo_contraro = int.Parse(Console.ReadLine());
+
+                //Base de cotización
+                    double base_de_cotizacion = salario_mensual * 0.4;
+                    if(base_de_cotizacion < 877803)
+                    {
+                        base_de_cotizacion = 877803;
+                        //salario minimo del 2020, nos hubiera dicho que era con ese desde antes :c
+                    }   
+                //Deducciones    
+                    double pension;
+                    double eps;
+                    double arl;
+                    double deducciones;
+                    double salario_real_menusal;
+                    double salario_anual;
+                
+                //Proceso
+                    if(tipo_contraro == 1)
+                    {
+                        pension = 0.04;
+                        eps = 0.04;
+                        deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps);               
+                        salario_real_menusal = salario_mensual - deducciones;
+                        Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                        salario_anual = salario_real_menusal * 12 + salario_mensual;
+                        Console.WriteLine("Su salario anual es. " + salario_anual);
+                    }
+                    else if(tipo_contraro == 2)
+                    {
+                        Console.WriteLine("Ingrese su riesgo laboral (del 1 al 5)");
+                        int riesgo_laboral = int.Parse(Console.ReadLine());
+                        pension = 0.16;
+                        eps = 0.125;
+                        if(riesgo_laboral == 1)
+                        {
+                            arl = 0.00522;
+                            deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps) + (base_de_cotizacion * arl);
+                            salario_real_menusal = salario_mensual - deducciones;
+                            salario_anual = salario_real_menusal * 12;
+                            Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                            Console.WriteLine("Su salario anual es. " + salario_anual);
+                        }
+                        if(riesgo_laboral == 2)
+                        {
+                            arl = 0.10144;
+                            deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps) + (base_de_cotizacion * arl);
+                            salario_real_menusal = salario_mensual - deducciones;
+                            salario_anual = salario_real_menusal * 12;
+                            Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                            Console.WriteLine("Su salario anual es. " + salario_anual);
+                        }
+                        if(riesgo_laboral == 3)
+                        {
+                            arl = 0.2436;
+                            deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps) + (base_de_cotizacion * arl);
+                            salario_real_menusal = salario_mensual - deducciones;
+                            salario_anual = salario_real_menusal * 12;
+                            Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                            Console.WriteLine("Su salario anual es. " + salario_anual);
+                        }
+                        if(riesgo_laboral == 4)
+                        {
+                            arl = 0.04350;
+                            deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps) + (base_de_cotizacion * arl);
+                            salario_real_menusal = salario_mensual - deducciones;
+                            salario_anual = salario_real_menusal * 12;
+                            Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                            Console.WriteLine("Su salario anual es. " + salario_anual);
+                        }
+                        if(riesgo_laboral == 5)
+                        {
+                            arl = 0.06960;
+                            deducciones = (base_de_cotizacion * pension) + (base_de_cotizacion * eps) + (base_de_cotizacion * arl);
+                            salario_real_menusal = salario_mensual - deducciones;
+                            salario_anual = salario_real_menusal * 12;
+                            Console.WriteLine("Su salario real mensual es: " + salario_real_menusal);
+                            Console.WriteLine("Su salario anual es. " + salario_anual);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ese valor no se encuentra en la clase de riesgo");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No cumple con ninguno de los tipos de contrato");
+                    }
+        }
+    }
+}
