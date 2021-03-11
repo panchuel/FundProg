1ac0e8c8ff983b3bbb701cd62c8700485650de79
panchuel <66889936+panchuel@users.noreply.github.com>
2021-03-10 21:09:30 -0500

    Add files via upload

---
+++ Desafio7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio7
+{
+    class Program
+    {
+        static void Main()
+        {
+            int vecesEncontrada = 0;
+            string frase = "", palabra = "";
+            
+            Console.WriteLine("Ingrese la frase deseada");              //Ingreso de frase
+            frase = Console.ReadLine();
+            frase = frase.ToUpper();
+
+            Console.WriteLine("Ingresa la palabra a buscar");           //Ingreso de palabra a buscar
+            palabra = Console.ReadLine();
+            palabra = palabra.ToUpper();
+
+            string[] palabrasTotales = frase.Split(" ");                //Separar la frase en un arreglo
+
+            for (int i = 0; i < palabrasTotales.Length; i++)
+            {
+                if(palabrasTotales[i] == palabra)                       //Contar las veces encontrada la palabra
+                {
+                    Console.WriteLine("Palabra encontrada en la posición " + i);
+                    vecesEncontrada++;
+                }
+            }
+            Console.WriteLine("Veces encontrada: " + vecesEncontrada);
+        }
+    }
+}
